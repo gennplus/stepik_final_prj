328794ce5a3dec04e1bbb4d8ccfe9fb48d473577
GennadyS <gennplus@gmail.com>
2022-09-26 21:37:32 +0200

    test that Login Page can be opened by link

--- test/pages/login_page.py
+++ test/pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.browser.current_url
+        assert "login" in self.url
 
     def should_be_login_form(self):
         assert len(self.browser.find_elements(*LoginPageLocators.LOGIN_FORM)) == 1, \
