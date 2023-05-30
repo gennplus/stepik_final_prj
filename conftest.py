91e780dd42cc595897c40a0eaa1386a89a96f1c3
GennadyS <gennplus@gmail.com>
2023-05-30 23:54:22 +0200

    Remove old commented code.

--- conftest.py
+++ conftest.py
@@ -1,15 +1,6 @@
 import pytest
 from selenium import webdriver
 
-# @pytest.fixture(scope="function")
-# def browser():
-#     print("\nstart browser for test..")
-#     browser = webdriver.Chrome()
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
-
-
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
